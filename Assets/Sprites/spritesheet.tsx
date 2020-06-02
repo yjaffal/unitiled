7fe97eeb8b0bd0130627026b6d812d2c910b294e
Yasser Jaffal <aljaffal@gmail.com>
2020-06-02 22:27:42 +0300

    added demo map

---
+++ Assets/Sprites/spritesheet.tsx
@@ -0,0 +1,89 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.5" name="spritesheet" tilewidth="21" tileheight="21" spacing="2" margin="2" tilecount="900" columns="30">
+ <image source="spritesheet.png" width="692" height="692"/>
+ <tile id="26">
+  <animation>
+   <frame tileid="26" duration="100"/>
+   <frame tileid="27" duration="100"/>
+   <frame tileid="28" duration="100"/>
+   <frame tileid="29" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="191">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0600686" y="0" width="20.964" height="20.964"/>
+  </objectgroup>
+ </tile>
+ <tile id="238">
+  <animation>
+   <frame tileid="238" duration="300"/>
+   <frame tileid="239" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="242">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0600686" y="0" width="20.8438" height="20.9039"/>
+  </objectgroup>
+ </tile>
+ <tile id="243">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0600686" y="0.0600686" width="20.8438" height="20.964"/>
+  </objectgroup>
+ </tile>
+ <tile id="244">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0600686" y="0.0600686" width="20.9039" height="20.8438"/>
+  </objectgroup>
+ </tile>
+ <tile id="246">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="21.024" y="1.08124">
+    <polygon points="0,0 -19.9428,19.8827 -0.0600686,19.8827"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="268">
+  <animation>
+   <frame tileid="268" duration="300"/>
+   <frame tileid="269" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="311">
+  <animation>
+   <frame tileid="311" duration="200"/>
+   <frame tileid="341" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="358">
+  <animation>
+   <frame tileid="358" duration="100"/>
+   <frame tileid="359" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="380">
+  <animation>
+   <frame tileid="380" duration="200"/>
+   <frame tileid="381" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="442">
+  <animation>
+   <frame tileid="441" duration="100"/>
+   <frame tileid="442" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="470">
+  <animation>
+   <frame tileid="470" duration="100"/>
+   <frame tileid="471" duration="100"/>
+   <frame tileid="472" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="596">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0600686" y="-0.0600686" width="20.7838" height="20.964">
+    <ellipse/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
